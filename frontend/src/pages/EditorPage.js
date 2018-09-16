cc653a99fe03c04e8cb2171a91afa9d8e3236a9c
JeongSJ <privacy_jch@naver.com>
2018-09-16 14:57:15 +0900

    03-5-2. EditorHeader Component

--- frontend/src/pages/EditorPage.js
+++ frontend/src/pages/EditorPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import EditorTemplate from 'components/editor/EditorTemplate';
+import EditorHeader from 'components/editor/EditorHeader';
 
 const EditorPage = () => {
     return (
         <EditorTemplate
-          header="헤더"
+          header={<EditorHeader />}
           preview="프리뷰"
           editor="에디터"
         />

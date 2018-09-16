95630083e7da5d6d055a69058bf9c2c076033aa8
JeongSJ <privacy_jch@naver.com>
2018-09-16 13:36:43 +0900

    03-3-1.  ListWrapper Component

--- frontend/src/pages/ListPage.js
+++ frontend/src/pages/ListPage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import PageTemplate from 'components/common/PageTemplate';
+import ListWrapper from 'components/list/ListWrapper';
 
 class ListPage extends Component {
     render() {
         return (
             <PageTemplate>
-                List
+                <ListWrapper>
+                    리스트
+                </ListWrapper>
             </PageTemplate>
         );
     }

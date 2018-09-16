85d4e0979497516ad5bd0fd0a2106171c7453f30
JeongSJ <privacy_jch@naver.com>
2018-09-16 14:14:26 +0900

    03-3-3. Pagination Component

--- frontend/src/pages/ListPage.js
+++ frontend/src/pages/ListPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PageTemplate from 'components/common/PageTemplate';
 import ListWrapper from 'components/list/ListWrapper';
 import PostList from 'components/list/PostList';
+import Pagination from 'components/list/Pagination';
 
 class ListPage extends Component {
     render() {
@@ -9,6 +10,7 @@ class ListPage extends Component {
             <PageTemplate>
                 <ListWrapper>
                     <PostList />
+                    <Pagination />
                 </ListWrapper>
             </PageTemplate>
         );

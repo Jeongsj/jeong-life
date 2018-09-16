a2b23cf5b6a41266b49d95dff6eb03f113d31daa
JeongSJ <privacy_jch@naver.com>
2018-09-16 15:01:20 +0900

    03-5-3. EditorPane Component

--- frontend/src/pages/EditorPage.js
+++ frontend/src/pages/EditorPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import EditorTemplate from 'components/editor/EditorTemplate';
 import EditorHeader from 'components/editor/EditorHeader';
+import EditorPane from 'components/editor/EditorPane';
 
 const EditorPage = () => {
     return (
         <EditorTemplate
           header={<EditorHeader />}
           preview="프리뷰"
-          editor="에디터"
+          editor={<EditorPane />}
         />
     );
 };

80e75b5b7e832572b2fe756b1301a250d0ab8bd4
JeongSJ <privacy_jch@naver.com>
2018-09-16 12:24:42 +0900

    03-1. FrontEnd Init Fin.
    Default Constructor Fin.

---
+++ frontend/src/store/modules/editor.js
@@ -0,0 +1,15 @@
+import { createAction, handleActions } from 'redux-actions';
+import { Map } from 'immutable';
+import { pender } from 'redux-pender';
+
+// action types
+
+// ction creators
+
+// init state
+const initialState = Map({});
+
+// reducer
+export default handleActions({
+
+}, initialState);

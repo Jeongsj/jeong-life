5577529fc5c3b5c8b7f8fa81a9a26ebac5bf37d8
JeongSJ <privacy_jch@naver.com>
2018-09-16 14:32:34 +0900

    03-5-0. Editor Component Init

---
+++ frontend/src/components/editor/EditorHeader/EditorHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './EditorHeader.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const EditorHeader = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EditorHeader;

db47b0d50b31730d63524cea61e45dabcc91e26d
JeongSJ <privacy_jch@naver.com>
2018-09-16 13:43:05 +0900

    03-3-2. PostList Component
    Warning
      - Included Dummy data
        (\frontend\src\components\list\PostList\PostList.js)

--- frontend/src/pages/ListPage.js
+++ frontend/src/pages/ListPage.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import PageTemplate from 'components/common/PageTemplate';
 import ListWrapper from 'components/list/ListWrapper';
+import PostList from 'components/list/PostList';
 
 class ListPage extends Component {
     render() {
         return (
             <PageTemplate>
                 <ListWrapper>
-                    리스트
+                    <PostList />
                 </ListWrapper>
             </PageTemplate>
         );
